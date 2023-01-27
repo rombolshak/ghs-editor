9cb0bb1c3b87544ce6da9f3a05b8a52013d617be
Roman Bolshakov <rombolshak@gmail.com>
2023-01-27 15:05:08 +0300

    scenario details models

--- src/app/core/models/scenario.models.ts
+++ src/app/core/models/scenario.models.ts
@@ -1,3 +1,8 @@
+import { ObjectiveData } from '@ghs/game/model/data/ObjectiveData';
+import { RoomData } from '@ghs/game/model/data/RoomData';
+import { ScenarioRule } from '@ghs/game/model/data/ScenarioRule';
+import { LootDeckConfig } from '@ghs/game/model/Loot';
+
 export interface Scenario {
   id: string;
   order: number;
@@ -10,6 +15,38 @@ export interface GeneralScenarioInfo {
   name: string;
   group: string;
   initial: boolean;
+}
+
+export interface ScenarioMonsters {
+  monsters: string[];
+  allies: string[];
+
+  drawExtra: string[]; // if two bosses in one scenario, each needs his own card from boss deck
+
+  allyDeck: boolean; // use separate deck for monster allies
+}
+
+export interface ScenarioRooms {
+  rooms: RoomData[];
+  marker: string;
+}
+
+export interface SpecialRules {
+  rules: ScenarioRule[];
+}
+
+export interface Section {
+  parent: string | undefined;
+  parentSections: string[];
+  resetRound: boolean;
+}
+
+export interface ScenarioProperties {
+  unlocks: string[];
+  blocks: string[];
+  requires: string[][];
+  lootDeckConfig: LootDeckConfig;
+  objectives: ObjectiveData[];
 }
 
 export const initialGeneralInfo: GeneralScenarioInfo = {
--- src/app/feature/scenarios/components/scenario-properties-editor/scenario-properties-editor.component.ts
+++ src/app/feature/scenarios/components/scenario-properties-editor/scenario-properties-editor.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { TuiDestroyService } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'ghse-scenario-properties-editor',
   templateUrl: './scenario-properties-editor.component.html',
   styleUrls: ['./scenario-properties-editor.component.less'],
 })
-export class ScenarioPropertiesEditorComponent {}
+export class ScenarioPropertiesEditorComponent {
+  constructor(activatedRoute: ActivatedRoute, formBuilder: NonNullableFormBuilder, destroy$: TuiDestroyService) {
+    const form = formBuilder.group({
+      index: ['', Validators.required],
+      group: '',
+      name: ['', Validators.required],
+      initial: false,
+    });
+  }
+}
